b5f4748e9d54fb56c68e28876ad37c7de5aeb937
2112942597 <57712846+2112942597@users.noreply.github.com>
2022-05-19 17:31:18 +0800

    Update model.py

--- ab/model.py
+++ ab/model.py
@@ -1,3 +1,4 @@
+#  If you want to get the detailed code, please contact lwf@haut.edu.cn
 import os
 import numpy as np
 import pandas as pd
@@ -39,96 +40,7 @@ backbone = EfficientNetB4(weights='imagenet',
                             input_shape=(256,256,3))
 backbone.layers[326].output,backbone.layers[149].output,backbone.layers[90].output,backbone.layers[31].output
 
-def convolution_block(x, filters, size, strides=(1,1), padding='same', activation=True):
-    x = Conv2D(filters, size, strides=strides, padding=padding)(x)
-    x = BatchNormalization()(x)
-    if activation == True:
-        x = LeakyReLU(alpha=0.1)(x)
-    return x
-
-def residual_block(Input, num_filters=16):
-    x = LeakyReLU(alpha=0.1)(Input)
-    x = BatchNormalization()(x)
-    blockInput = BatchNormalization()(Input)
-    x = convolution_block(x, num_filters, (3,3) )
-    x = convolution_block(x, num_filters, (3,3), activation=False)
-    x = Add()([x, Input])
-    return x
-
-
-from tensorflow.keras.applications.efficientnet import EfficientNetB4
-
-
-def U_Net(input_shape=(None, None, 3), dropout_rate=0.25):
-    backbone = EfficientNetB4(weights='imagenet',
-                              include_top=False,
-                              input_shape=input_shape)
-    input = backbone.input
-    start_neurons = 16
-
-    conv4 = backbone.layers[326].output                                        #16
-    conv4 = LeakyReLU(alpha=0.1)(conv4)
-    pool4 = MaxPooling2D((2, 2))(conv4)                                         #8
-    pool4 = Dropout(dropout_rate)(pool4)
-
-
-    convm = Conv2D(start_neurons * 32, (3, 3), activation=None, padding="same")(pool4)
-    convm = residual_block(convm, start_neurons * 32)
-    convm = residual_block(convm, start_neurons * 32)
-    convm = LeakyReLU(alpha=0.1)(convm)
-
-    deconv4 = Conv2DTranspose(start_neurons * 16, (3, 3), strides=(2, 2), padding="same")(convm)
-    uconv4 = concatenate([deconv4, conv4])
-    uconv4 = Dropout(dropout_rate)(uconv4)
-
-    uconv4 = Conv2D(start_neurons * 16, (3, 3), activation=None, padding="same")(uconv4)
-    uconv4 = residual_block(uconv4, start_neurons * 16)
-    uconv4 = residual_block(uconv4, start_neurons * 16)
-    uconv4 = LeakyReLU(alpha=0.1)(uconv4)
-
-    deconv3 = Conv2DTranspose(start_neurons * 8, (3, 3), strides=(2, 2), padding="same")(uconv4)          #32
-    conv3 = backbone.layers[149].output
-    uconv3 = concatenate([deconv3, conv3])
-    uconv3 = Dropout(dropout_rate)(uconv3)
-
-    uconv3 = Conv2D(start_neurons * 8, (3, 3), activation=None, padding="same")(uconv3)
-    uconv3 = residual_block(uconv3, start_neurons * 8)
-    uconv3 = residual_block(uconv3, start_neurons * 8)
-    uconv3 = LeakyReLU(alpha=0.1)(uconv3)
-
-    deconv2 = Conv2DTranspose(start_neurons * 4, (3, 3), strides=(2, 2), padding="same")(uconv3)         #64
-    conv2 = backbone.layers[90].output
-    uconv2 = concatenate([deconv2, conv2])
-
-    uconv2 = Dropout(0.1)(uconv2)
-    uconv2 = Conv2D(start_neurons * 4, (3, 3), activation=None, padding="same")(uconv2)
-    uconv2 = residual_block(uconv2, start_neurons * 4)
-    uconv2 = residual_block(uconv2, start_neurons * 4)
-    uconv2 = LeakyReLU(alpha=0.1)(uconv2)
-
-    deconv1 = Conv2DTranspose(start_neurons * 2, (3, 3), strides=(2, 2), padding="same")(uconv2)                    #128
-    conv1 = backbone.layers[31].output
-    uconv1 = concatenate([deconv1, conv1])
-
-    uconv1 = Dropout(0.1)(uconv1)
-    uconv1 = Conv2D(start_neurons * 2, (3, 3), activation=None, padding="same")(uconv1)
-    uconv1 = residual_block(uconv1, start_neurons * 2)
-    uconv1 = residual_block(uconv1, start_neurons * 2)
-    uconv1 = LeakyReLU(alpha=0.1)(uconv1)
-
-    uconv0 = Conv2DTranspose(start_neurons * 1, (3, 3), strides=(2, 2), padding="same")(uconv1)                      #256
-    uconv0 = Dropout(0.1)(uconv0)
-    uconv0 = Conv2D(start_neurons * 1, (3, 3), activation=None, padding="same")(uconv0)
-    uconv0 = residual_block(uconv0, start_neurons * 1)
-    uconv0 = residual_block(uconv0, start_neurons * 1)
-    uconv0 = LeakyReLU(alpha=0.1)(uconv0)
-
-    uconv0 = Dropout(dropout_rate / 2)(uconv0)
-    output_layer = Conv2D(1, (1, 1), padding="same", activation="sigmoid")(uconv0)                                         #256*256*1
-
-    model = Model(input, output_layer)
-
-    return model
+........
 
 K.clear_session()
 img_size = 256
